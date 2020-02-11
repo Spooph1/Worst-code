ce6da5c0be2f6cf937f9f978d95e6d3b18e0e399
Simon Juul <simon@documentary.dk>
2020-02-11 17:54:03 +0100

    Made some bad code

---
+++ Worst Code/Worst Code/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worst_Code
+{
+    class Program
+    {
+        int varibIeoftypeInt;int varibleoftypeInt;int varibleOftypeInt; int varibleOfTypeInt;int varibleofTypelnt;int varibleofTypeInt;
+        static void Main(string[] args)
+        {
+            
+        }
+        int bigmathifthevaluesarethesameisheventhoughtheyaremadetobeasincomprehensiveasposssible(int varibIeofTypelnt){if(varibleOftypeInt==varibleOfTypeInt||varibleofTypelnt==varibleofTypeInt){varibIeofTypelnt=((varibleOfTypeInt+varibleofTypelnt)*3)+((varibleOftypeInt+varibleofTypeInt)*3)-((varibleOftypeInt+varibleofTypelnt)*3)-((varibleOfTypeInt+varibleofTypeInt)*3);return varibIeofTypelnt;}else{return varibIeofTypelnt=63;}}
+        int bigmathofsomesortwhichismadetobeasincomprehensibleaspossible(int varibleoftypelnt){if(varibIeoftypeInt==varibleoftypeInt){varibleoftypelnt=(varibIeoftypeInt+varibleoftypeInt)*3;return varibleoftypelnt;}else{varibleoftypelnt=varibIeoftypeInt+varibleoftypeInt;return varibleoftypelnt;}}
+    }
+}
